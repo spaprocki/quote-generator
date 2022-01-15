0586aef6e4629352ffd7fe26b995aadf05c77889
Szymon Paprocki <s.paprocki.vs@gmail.com>
2022-01-15 18:23:22 +0100

    Added layout for different medias

--- src/App.js
+++ src/App.js
@@ -23,24 +23,22 @@ class App extends React.Component {
             })
     }
 
-    textTransition = () => {
-    }
 
     render() {
         return (
             <div className="app">
-                <Card bg='info'>
+                <Card bg='info' className="col-lg-4 col-md-6 col-sm-8 col-10">
                     <Card.Header>Quote machine</Card.Header>
                     <Card.Body>
                         <ReactCSSTransitionReplace transitionName="fade-wait"
                            transitionEnterTimeout={1000} transitionLeaveTimeout={400}>
-                            <Card.Text key={this.state.advice} className="mb-0 text-justify" onChange={this.textTransition}>{this.state.advice}</Card.Text>
+                            <Card.Text key={this.state.advice} className="mb-0 text-justify">{this.state.advice}</Card.Text>
                         </ReactCSSTransitionReplace>
                     </Card.Body>
                     <Card.Footer>
                         <Stack className="justify-content-end" direction="horizontal" gap={1}>
-                            <Button id="new-quote" variant="primary" onClick={this.fetchAdvice}>New quote</Button>
-                            <Button id="tweet-quote" variant="primary" href="https://www.twitter.com/intent/tweet">Tweet quote</Button>
+                            <Button id="new-quote" variant="info" border="primary" onClick={this.fetchAdvice}>New quote</Button>
+                            <Button id="tweet-quote" variant="info" href="https://www.twitter.com/intent/tweet">Tweet quote</Button>
                         </Stack>
                     </Card.Footer>
                 </Card>
