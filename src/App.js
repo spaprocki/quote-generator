f93c7c8885ffb070043bc87b82343d2ddbab6fe6
Szymon Paprocki <s.paprocki.vs@gmail.com>
2022-01-15 17:27:37 +0100

    I've merged this project with a previous one

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 import './App.css';
+import { Button, Card, Stack } from 'react-bootstrap';
 
 class App extends React.Component {
     state = { advice: '' };
@@ -12,27 +13,36 @@ class App extends React.Component {
 
     fetchAdvice = () => {
         axios.get('https://api.adviceslip.com/advice')
-        .then((response) => {
-            const { advice } = response.data.slip
-            this.setState({advice})
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            .then((response) => {
+                const { advice } = response.data.slip
+                this.setState({ advice })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
-    
+
+    textTransition = () => {
+    }
+
     render() {
         return (
-<div className="app">
-    <div className="card">
-        <h1 className="header">{this.state.advice}</h1>
-        <button className="button" onClick={this.fetchAdvice}>
-            <span>Give me advice!</span>
-        </button>
-    </div>
-</div>
+            <div className="app">
+                <Card bg='info'>
+                    <Card.Header>Quote machine</Card.Header>
+                    <Card.Body>
+                        <Card.Text className="mb-0 text-justify" onChange={this.textTransition}>{this.state.advice}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Stack className="justify-content-end" direction="horizontal" gap={1}>
+                        <Button id="new-quote" variant="primary" onClick={this.fetchAdvice}>New quote</Button>
+                            <Button id="tweet-quote" variant="primary" href="https://www.twitter.com/intent/tweet">Tweet quote</Button>
+                        </Stack>
+                    </Card.Footer>
+                </Card>
+            </div>
 
-            
+
         )
     }
 }
